d3da6c02e2a2f2f1ff05e80b5027baf9b612b92d
Gustavo Dantas Botelho de Oliveira <96527439+Gustavo150303@users.noreply.github.com>
2023-05-31 10:59:34 -0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+const superrobo = document.querySelector("#galvatron")
+
+superrobo.addEventListener("click", function(){
+    console.log("Click no Galvatron")
+})
+    
+//Java = Clicar/Fazer alguma coisa
+
+function dizOi(){
+    console.log("Olá")
+    console.log("Bem-Vindo ao Galvatron 2023")
+}
+
+dizOi()
