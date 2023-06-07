9cc627640a4c05f5b77a1c4774e99dac70e6b399
Gustavo Dantas Botelho de Oliveira <96527439+Gustavo150303@users.noreply.github.com>
2023-06-07 09:44:40 -0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,30 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach((elemento) =>{
+    elemento.addEventListener("click", () => {
+        console.log("funcionou")
+    })    
 })
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1
+
+somar.addEventListener("click", () => { //aron function é como um return
+    //braco.value = parseInt(braco.value) + 1
+    manipulaDados("somar")
 })
+
+subtrair.addEventListener("click", () => {
+    //braco.value = parseInt(braco.value) - 1
+    manipulaDados("subtrair")
+})
+
+function manipulaDados(operacao){
+    if(operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
+}
 
