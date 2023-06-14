415f205aee0dddb82fec3286108b93e9586d36ec
Gustavo Dantas Botelho de Oliveira <96527439+Gustavo150303@users.noreply.github.com>
2023-06-14 11:13:19 -0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,50 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
-
-const controle = document.querySelectorAll(".controle-ajuste")
-
-controle.forEach((elemento) =>{
-    elemento.addEventListener("click", () => {
-        console.log("funcionou")
-    })    
-})
-
-
-somar.addEventListener("click", () => { //aron function é como um return
-    //braco.value = parseInt(braco.value) + 1
-    manipulaDados("somar")
-})
-
-subtrair.addEventListener("click", () => {
-    //braco.value = parseInt(braco.value) - 1
-    manipulaDados("subtrair")
-})
-
-function manipulaDados(operacao){
-    if(operacao === "subtrair"){
-        braco.value = parseInt(braco.value) - 1
-    }else{
-        braco.value = parseInt(braco.value) + 1
+const controle = document.querySelectorAll("[data-controle]")
+const pecas = {
+    "bracos":{
+        "forca": 29,
+        "poder": 29,
+        "energia": -21,
+        "velocidade":-5
+    },
+    "blindagem":{
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade":-20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade":-4
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade":43
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade":-2
     }
 }
 
+controle.forEach((elemento) =>{
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        
+    }) 
+})
+
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1
+    }else{
+        peca.value = parseInt(peca.value) + 1
+    }
+}
